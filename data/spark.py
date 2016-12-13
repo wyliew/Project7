566492e65d13e8f94d4c9cce8a28d083c26f5eb9
Wylie <ww5ts@virginia.edu>
2016-12-13 16:26:25 -0500

    Reduced data to 200 lines, added click_pairs to spark

--- data/spark.py
+++ data/spark.py
@@ -1,17 +1,25 @@
 from pyspark import SparkContext
+from itertools import combinations
+
+def getCombinations(line):
+	return [ (x[0], x[1]) for x in combinations(line[1],2)]
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-data = sc.textFile("/tmp/data/clickdata.tsv", 2)     # each worker loads a piece of the data file
+data = sc.textFile("/tmp/data/clickdata.tsv", 2).distinct()     # each worker loads a piece of the data file
 
 pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = pages.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
+lists = pairs.groupByKey()
+click_pairs = lists.flatMap(getCombinations)
+#pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
+#count = pages.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
                                                   # and then reduce all the values by adding them together
 
-output = count.collect()                          # bring the data back to the master node so we can print it out
-for page_id, count in output:
-    print ("page_id %s count %d" % (page_id, count))
-print ("Popular items done")
+output = click_pairs.collect()                          # bring the data back to the master node so we can print it out
+for page_id, l in output:
+	print(page_id + ':', end='')
+	for elem in l:
+		print(elem, end=',')
+	print()
 
 sc.stop()
